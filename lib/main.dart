59db185330e8b1417c5c2a8a50e08facd938f26a
edgvac2298 <edgardovac2298@gmail.com>
2022-05-03 15:05:54 -0500

    Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,14 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(//const MyHomePage(title: 'Flutter Demo Home Page'),
+        appBar: AppBar(
+          title: const Text("Hola Mundo"),
+        ),
+        body: const Center(
+          child: Text("Hola Mundo"),
+        ),
+      )
     );
   }
 }
